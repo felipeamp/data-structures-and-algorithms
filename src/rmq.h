bdb5efd1e00feccf5e7c923ca8acd752383a2a95
Felipe de A. Mello Pereira <felipeamp@gmail.com>
2017-06-12 12:16:19 -0300

    Corrected bugs in implementation. Still missing Fischer-Heun.

--- src/rmq.h
+++ src/rmq.h
@@ -5,8 +5,12 @@
 
 #include <algorithm>
 #include <cmath>
+#include <memory>
 #include <unordered_map>
 #include <vector>
+
+
+#include <iostream>
 
 namespace rmq {
 
@@ -32,17 +36,10 @@ public:
   explicit Sparse(const std::vector<T> &vec) {
     vec_ = std::vector<T>(vec.begin(), vec.end());
 
-    log2_.push_back(0);
-    size_t log = 0;
-    for (size_t slice_size = 1; slice_size < vec.size(); slice_size *= 2, ++log) {
-      std::fill_n(log2_.end(), slice_size, log);
-    }
-
-    this->block_min_ = std::vector<std::unordered_map<size_t, size_t>>;
+    block_min_ = std::vector<std::unordered_map<size_t, size_t>>(vec_.size());
     block_min_.reserve(vec_.size());
     for (size_t index = 0; index < vec_.size(); ++index) {
-      block_min_.push_back(std::unordered_map<size_t, size_t>());
-      block_min_[index].reserve(log2_[vec_.size()]);
+      block_min_.emplace_back();
       block_min_[index][1] = index;
     }
     for (size_t block_size = 2; block_size <= vec_.size(); block_size *= 2) {
@@ -59,7 +56,7 @@ public:
   }
 
   size_t query(size_t start, size_t end) {
-    size_t block_size = log2_[end - start];
+    size_t block_size = (end - start + 1) / 2;
     size_t min_index_1 = block_min_[start][block_size];
     size_t min_index_2 = block_min_[end - block_size][block_size];
     if (vec_[min_index_1] <= vec_[min_index_2])
@@ -70,7 +67,6 @@ public:
 
 private:
   std::vector<std::unordered_map<size_t, size_t>> block_min_;
-  std::vector<size_t> log2_;
   std::vector<T> vec_;
 };
 
@@ -81,6 +77,7 @@ public:
     vec_ = std::vector<T>(vec.begin(), vec.end());
 
     block_size_ = std::floor(std::log2(vec_.size()));
+
     size_t num_blocks = std::ceil(
       static_cast<double>(vec_.size()) / static_cast<double>(block_size_));
 
@@ -96,7 +93,7 @@ public:
       std::min_element(vec_.begin() + (num_blocks - 1) * block_size_, vec_.end()) - vec_.begin());
     high_blocks[num_blocks - 1] = vec_[block_min_[num_blocks - 1]];
 
-    rmq_high_blocks_ = Sparse<T>(high_blocks);
+    rmq_high_blocks_ = std::make_unique<rmq::Sparse<T>>(high_blocks);
   }
 
   size_t query(size_t start, size_t end) {
@@ -105,7 +102,7 @@ public:
     if (end_block_num <= start_block_num + 1) {
       return std::min_element(vec_.begin() + start, vec_.begin() + end) - vec_.begin();
     } else {
-      size_t min_index_high = rmq_high_blocks_.query(start_block_num + 1, end_block_num);
+      size_t min_index_high = rmq_high_blocks_->query(start_block_num + 1, end_block_num);
       size_t min_index = block_min_[min_index_high];
       for (size_t index = start; index < (start_block_num + 1) * block_size_; ++index) {
         if (vec_[index] < vec_[min_index]) {
@@ -123,7 +120,7 @@ public:
 
 private:
   size_t block_size_;
-  Sparse<T> rmq_high_blocks_;
+  std::unique_ptr<rmq::Sparse<T>> rmq_high_blocks_;
   std::vector<size_t> block_min_;
   std::vector<T> vec_;
 };
--- src/rmq_test.cc
+++ src/rmq_test.cc
@@ -1,5 +1,6 @@
 // Copyright 2017 <felipeamp>
 
+#include <iostream>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -7,7 +8,13 @@
 
 namespace {
 
-TEST(RMQ, FirstElem) {
+TEST(RMQ, NaivePreProcessing) {
+  std::vector<double> v = {1., 2., 3., 4.};
+
+  rmq::Naive<double> naive(v);
+}
+
+TEST(RMQ, NaiveQuery) {
   std::vector<double> v = {1., 2., 3., 4.};
 
   rmq::Naive<double> naive(v);
@@ -15,25 +22,48 @@ TEST(RMQ, FirstElem) {
   ASSERT_EQ(0, naive.query(0, 2));
   ASSERT_EQ(1, naive.query(1, v.size()));
   ASSERT_EQ(2, naive.query(2, v.size()));
+}
+
+TEST(RMQ, SparsePreProcessing) {
+  std::vector<double> v = {1., 2., 3., 4.};
 
   rmq::Sparse<double> sparse(v);
-  // ASSERT_EQ(0, sparse.query(0, v.size()));
-  // ASSERT_EQ(0, sparse.query(0, 2));
-  // ASSERT_EQ(1, sparse.query(1, v.size()));
-  // ASSERT_EQ(2, sparse.query(2, v.size()));
+}
 
-  // rmq::Hybrid<double> hybrid(v);
-  // ASSERT_EQ(0, hybrid.query(0, v.size()));
-  // ASSERT_EQ(0, hybrid.query(0, 2));
-  // ASSERT_EQ(1, hybrid.query(1, v.size()));
-  // ASSERT_EQ(2, hybrid.query(2, v.size()));
+TEST(RMQ, SparseQuery) {
+  std::vector<double> v = {1., 2., 3., 4.};
 
+  rmq::Sparse<double> sparse(v);
+  ASSERT_EQ(0, sparse.query(0, v.size()));
+  ASSERT_EQ(0, sparse.query(0, 2));
+  ASSERT_EQ(1, sparse.query(1, v.size()));
+  ASSERT_EQ(2, sparse.query(2, v.size()));
+}
+
+TEST(RMQ, HybridPreProcessing) {
+  std::vector<double> v = {1., 2., 3., 4.};
+
+  rmq::Hybrid<double> hybrid(v);
+}
+
+TEST(RMQ, HybridQuery) {
+  std::vector<double> v = {1., 2., 3., 4.};
+
+  rmq::Hybrid<double> hybrid(v);
+  ASSERT_EQ(0, hybrid.query(0, v.size()));
+  ASSERT_EQ(0, hybrid.query(0, 2));
+  ASSERT_EQ(1, hybrid.query(1, v.size()));
+  ASSERT_EQ(2, hybrid.query(2, v.size()));
+}
+
+// TEST(RMQ, FischerHeun) {
+//   std::vector<double> v = {1., 2., 3., 4.};
   // rmq::FischerHeun<double> fischer_heun(v);
   // ASSERT_EQ(0, fischer_heun.query(0, v.size()));
   // ASSERT_EQ(0, fischer_heun.query(0, 2));
   // ASSERT_EQ(1, fischer_heun.query(1, v.size()));
   // ASSERT_EQ(2, fischer_heun.query(2, v.size()));
-}
+// }
 
 }  // namespace
 
